60b747c8e13930fe78ffc47b90ca97e69a0cb8e5
Nicholas Saquilayan <nsaquilayan@berkeley.edu>
2020-09-07 21:30:03 -0700

    change port number to environment or default

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const axios = require("axios");
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 
